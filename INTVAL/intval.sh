8db06a38209f86437265fcd4ef361e78915c2104
Jakub Pachciarek <kuba@pachciarek.pl>
2017-02-05 14:51:59 +0100

    0 delay added

--- INTVAL/intval.sh
+++ INTVAL/intval.sh
@@ -28,13 +28,14 @@ do
   sleep 0.5
   echo 0 > /sys/devices/platform/leds-gpio/leds/card/brightness
 
-  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/2.png "sh /mnt/mmc/INTVAL/timelapse.sh 2 &" 1 112 64 64 &
-  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/5.png "sh /mnt/mmc/INTVAL/timelapse.sh 5 &" 1 176 64 64 &
-  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/10.png "sh /mnt/mmc/INTVAL/timelapse.sh 10 &" 1 240 64 64 &
-  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/15.png "sh /mnt/mmc/INTVAL/timelapse.sh 15 &" 1 304 64 64 &
-  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/30.png "sh /mnt/mmc/INTVAL/timelapse.sh 30 &" 1 368 64 64 &
-  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/45.png "sh /mnt/mmc/INTVAL/timelapse.sh 45 &" 1 432 64 64 &
-  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/60.png "sh /mnt/mmc/INTVAL/timelapse.sh 60 &" 1 496 64 64 &
-  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/90.png "sh /mnt/mmc/INTVAL/timelapse.sh 90 &" 1 560 64 64 &
-  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/X.png "sleep 0.1; killall button_png" 1 624 64 64 &
+  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/0.png "sh /mnt/mmc/INTVAL/timelapse.sh 0 &" 1 80 64 64 &
+  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/2.png "sh /mnt/mmc/INTVAL/timelapse.sh 2 &" 1 144 64 64 &
+  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/5.png "sh /mnt/mmc/INTVAL/timelapse.sh 5 &" 1 208 64 64 &
+  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/10.png "sh /mnt/mmc/INTVAL/timelapse.sh 10 &" 1 272 64 64 &
+  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/15.png "sh /mnt/mmc/INTVAL/timelapse.sh 15 &" 1 336 64 64 &
+  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/30.png "sh /mnt/mmc/INTVAL/timelapse.sh 30 &" 1 400 64 64 &
+  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/45.png "sh /mnt/mmc/INTVAL/timelapse.sh 45 &" 1 464 64 64 &
+  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/60.png "sh /mnt/mmc/INTVAL/timelapse.sh 60 &" 1 528 64 64 &
+  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/90.png "sh /mnt/mmc/INTVAL/timelapse.sh 90 &" 1 592 64 64 &
+  /mnt/mmc/INTVAL/button_png /mnt/mmc/INTVAL/X.png "sleep 0.1; killall button_png" 1 656 64 64 &
 done
